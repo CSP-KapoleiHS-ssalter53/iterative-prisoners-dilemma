b4ae2e0a406c5086e9b1281d2078e3f54645cd72
IsaiahSilva <34248199+IsaiahSilva@users.noreply.github.com>
2017-12-14 08:19:07 -1000

    Update team11.py

--- team11.py
+++ team11.py
@@ -1,56 +1,15 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
-
-team_name = 'Team Luke' # Only 10 chars displayed.
-strategy_name = 'The unknown strategy'
-strategy_description = 'If their history is b then we will betray otherwise we collude'
-    
-def move(my_history, their_history, my_score, their_score):
-
-    if 'b' in their_history:
+import random 
+team_name = 'team11'
+strategy_name = 'SneakyBastartd+Crook/BullesyeAce+Armorer'
+strategy_description = 'Yeet'
+def move(my_history, their_history, my_score, their_score): #Startegy name: The legend 28
+    if len(my_history) == 0:
+        return 'c' #Returns Collude on the first round
+    elif my_history[-1:101]=='c' and their_history[-1:101] =='b': #If they betray we betray the next round.
         return 'b'
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
     else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+        return 'c'
+    if their_history == 'cc':
+        return 'b'
+    if my_history % 5 == 0: 
+        return random.choice(['b','c'])
